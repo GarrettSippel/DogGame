a0bbcb92184c08d4f02984ff85868636189f56ec
GarrettSippel <gasi9744@colorado.edu>
2017-02-14 11:29:16 -0700

    commit to a branch
    im commiting to a branch

--- Dog/Assets/scripts/playablecharacter.cs
+++ Dog/Assets/scripts/playablecharacter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class playablecharacter : MonoBehaviour {
 
-    public int hungers = 100;
+    public int hunger = 100;
 
     public Rigidbody rb;
     public float speed = 200f;
@@ -29,7 +29,7 @@ public class playablecharacter : MonoBehaviour {
         xMov = -(Mathf.Sin((Mathf.PI) * ((nose.z)) / 180));
         yMov = Mathf.Cos((Mathf.PI)*((nose.z)) /180);
 
-        rotateInput = rotateInput + ((Input.GetAxis("Horizontal") * rotateSpeed));
+        rotateInput = rotateInput + (-(Input.GetAxis("Horizontal") * rotateSpeed));
         Quaternion rotate = Quaternion.Euler(0.0f,0.0f, rotateInput);
         float moveForward = Input.GetAxis("Vertical")*speed;
         Vector3 movement = new Vector3(xMov*moveForward, yMov*moveForward, 0.0f);
